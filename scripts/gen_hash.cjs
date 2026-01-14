a0dfd090964009df9548f7c1529bde7392b0b0e8
Bapi Raju <mooments_admin@volyty.com>
2026-01-14 14:43:33 +0530

    chore: remove test artifacts from version control and update .gitignore

--- scripts/gen_hash.cjs
+++
@@ -1,6 +0,0 @@
-const { scryptSync, randomBytes } = require('crypto');
-const pass = process.argv[2];
-if (!pass) { console.error('Usage: node gen_hash.cjs <password>'); process.exit(1); }
-const salt = randomBytes(16).toString('hex');
-const buf = scryptSync(pass, salt, 64);
-console.log(buf.toString('hex') + '.' + salt);
